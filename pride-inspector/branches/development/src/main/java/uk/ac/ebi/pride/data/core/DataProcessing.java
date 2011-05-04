1182061f25d932f4e513356533e814f67eb61b44
harry.r.wang <harry.r.wang@gmail.com>
2011-05-04 12:49:29 +0000

    creating a development branch

---
+++ pride-inspector/branches/development/src/main/java/uk/ac/ebi/pride/data/core/DataProcessing.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.pride.data.core;
+
+import java.util.List;
+
+/**
+ * Description of the way in which a particular software was used.
+ * <p/>
+ * User: rwang
+ * Date: 04-Feb-2010
+ * Time: 16:04:58
+ */
+public class DataProcessing implements MassSpecObject {
+
+    /**
+     * unique identifier for this data processing object
+     */
+    private String id = null;
+    /**
+     * description of the default peak processing method, this is a ordered List
+     */
+    private List<ProcessingMethod> processingMethods = null;
+
+    /**
+     * Constructor
+     *
+     * @param id      required.
+     * @param methods required and cannot be empty.
+     */
+    public DataProcessing(String id, List<ProcessingMethod> methods) {
+        setId(id);
+        setProcessingMethods(methods);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<ProcessingMethod> getProcessingMethods() {
+        return processingMethods;
+    }
+
+    public void setProcessingMethods(List<ProcessingMethod> processingMethods) {
+        // in PRIDE XML, processing methods can be null or empty.
+        // in mzML however, processing methods should be always available.
+        this.processingMethods = processingMethods;
+    }
+}
---
+++ pride-inspector/branches/development/src/main/java/uk/ac/ebi/pride/gui/task/impl/UpdateForegroundEntryTask.java
@@ -0,0 +1,49 @@
+package uk.ac.ebi.pride.gui.task.impl;
+
+import uk.ac.ebi.pride.data.controller.DataAccessController;
+import uk.ac.ebi.pride.data.core.Chromatogram;
+import uk.ac.ebi.pride.data.core.Experiment;
+import uk.ac.ebi.pride.data.core.Identification;
+import uk.ac.ebi.pride.data.core.Spectrum;
+
+/**
+ * Retrieve foreground entry using data access controller
+ * 
+ * User: rwang
+ * Date: 01-Mar-2010
+ * Time: 16:15:08
+ */
+public class UpdateForegroundEntryTask<T> extends AbstractDataAccessTask<T, Void> {
+
+    private Comparable id = null;
+    private Class<T> classType = null;
+
+    public UpdateForegroundEntryTask(DataAccessController controller, Class<T> classType, Comparable id) {
+        super(controller);
+        this.id = id;
+        this.classType = classType;
+        this.setName("Loading " + classType.getSimpleName());
+        this.setDescription("Loading " + classType.getSimpleName() + "[ID: " + id + "]");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected T runDataAccess() throws Exception {
+        Object result = null;
+        if (Experiment.class.equals(classType)) {
+            controller.setForegroundExperimentAcc(id);
+            result = controller.getForegroundExperimentAcc();
+        } else if (Spectrum.class.equals(classType)) {
+            controller.setForegroundSpectrumById(id);
+            result = controller.getForegroundSpectrum();
+        } else if (Chromatogram.class.equals(classType)) {
+            controller.setForegroundChromatogramById(id);
+            result = controller.getForegroundChromatogram();
+        } else if (Identification.class.equals(classType)) {
+            controller.setForegroundIdentificationById(id);
+            result = controller.getForegroundIdentification();
+        }
+        
+        return (T)result;
+    }
+}

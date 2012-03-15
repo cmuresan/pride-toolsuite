f8d03aa4fcfce38fcb3c45a06c1cc71f8da7225f
ypriverol <ypriverol@df3d61a3-6ab4-724c-a686-eaaef7b5d8fe>
2012-03-15 17:57:55 +0000

    mzData integration

--- ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/controller/impl/MzDataControllerImpl.java
+++ ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/controller/impl/MzDataControllerImpl.java
@@ -6,23 +6,19 @@ import uk.ac.ebi.pride.data.controller.DataAccessController;
 import uk.ac.ebi.pride.data.controller.DataAccessException;
 import uk.ac.ebi.pride.data.controller.DataAccessMode;
 import uk.ac.ebi.pride.data.controller.cache.CacheCategory;
-import uk.ac.ebi.pride.data.controller.cache.impl.MzXmlCacheBuilder;
+import uk.ac.ebi.pride.data.controller.cache.impl.MzDataCacheBuilder;
 import uk.ac.ebi.pride.data.core.*;
-import uk.ac.ebi.pride.data.core.Chromatogram;
 import uk.ac.ebi.pride.data.core.DataProcessing;
-import uk.ac.ebi.pride.data.core.InstrumentConfiguration;
-import uk.ac.ebi.pride.data.core.ParamGroup;
+import uk.ac.ebi.pride.data.core.Person;
 import uk.ac.ebi.pride.data.core.Software;
 import uk.ac.ebi.pride.data.core.SourceFile;
 import uk.ac.ebi.pride.data.core.Spectrum;
-import uk.ac.ebi.pride.data.io.file.MzXmlUnmarshallerAdaptor;
+import uk.ac.ebi.pride.data.io.file.MzDataUnmarshallerAdaptor;
 import uk.ac.ebi.pride.data.utils.MD5Utils;
 import uk.ac.ebi.pride.tools.jmzreader.JMzReaderException;
-import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLFile;
-import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLParsingException;
-import uk.ac.ebi.pride.tools.mzxml_parser.mzxml.model.MsInstrument;
-import uk.ac.ebi.pride.tools.mzxml_parser.mzxml.model.Operator;
-import uk.ac.ebi.pride.tools.mzxml_parser.mzxml.model.ParentFile;
+import uk.ac.ebi.pride.tools.mzdata_parser.MzDataFile;
+import uk.ac.ebi.pride.tools.mzdata_parser.mzdata.model.*;
+
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -34,24 +30,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * User: yperez, rwang
- * Date: 2/27/12
- * Time: 2:12 PM
- *
+ * Created by IntelliJ IDEA.
+ * User: yperez
+ * Date: 3/15/12
+ * Time: 8:17 AM
+ * To change this template use File | Settings | File Templates.
  */
+public class MzDataControllerImpl extends CachedDataAccessController {
 
-public class MzXmlControllerImpl extends CachedDataAccessController {
-
-    private static final Logger logger = LoggerFactory.getLogger(MzXmlControllerImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(MzDataControllerImpl.class);
     /**
      * Pattern for validating mzML format
      */
-    private static Pattern mzXmlHeaderPattern = Pattern.compile("^(<\\?xml [^>]*>\\s*(<!--[^>]*-->\\s*)*){0,1}<(mzXML) xmlns=.*", Pattern.MULTILINE);
+    private static Pattern mzDataHeaderPattern = Pattern.compile("^(<\\?xml [^>]*>\\s*(<!--[^>]*-->\\s*)*){0,1}<(mzData) xmlns=.*", Pattern.MULTILINE);
 
     /**
      * Reader for getting information from jmzReader file
      */
-    private MzXmlUnmarshallerAdaptor unmarshaller;
+    private MzDataUnmarshallerAdaptor unmarshaller;
 
     /**
      * Construct a data access controller using a given mzML file
@@ -59,7 +55,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
      * @param file jmzReader file
      * @throws uk.ac.ebi.pride.data.controller.DataAccessException data access exception
      */
-    public MzXmlControllerImpl(File file) throws DataAccessException {
+    public MzDataControllerImpl(File file) throws DataAccessException {
         this(file, null);
     }
 
@@ -70,7 +66,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
      * @param mode data access mode
      * @throws DataAccessException data access exception
      */
-    public MzXmlControllerImpl(File file, DataAccessMode mode) throws DataAccessException {
+    public MzDataControllerImpl(File file, DataAccessMode mode) throws DataAccessException {
         super(file, mode);
         initialize();
     }
@@ -81,34 +77,36 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
      * @throws DataAccessException data access exception
      */
     private void initialize() throws DataAccessException {
-
         File file = (File) this.getSource();
         // create unmarshaller
-        MzXMLFile um = null;
+        MzDataFile um = null;
         try {
-            um = new MzXMLFile(file);
-            unmarshaller = new MzXmlUnmarshallerAdaptor(um);
-        } catch (MzXMLParsingException e) {
-            String msg = "Error while trying to initialize the mzXML file";
-            logger.error(msg, e);
-            throw new DataAccessException(msg, e);
-        }
+            um = new MzDataFile(file);
+            unmarshaller = new MzDataUnmarshallerAdaptor(um);
 
-        // set data source name
-        this.setName(file.getName());
-        // set the type
-        this.setType(DataAccessController.Type.XML_FILE);
-        // set the content categories
-        this.setContentCategories(DataAccessController.ContentCategory.SPECTRUM,
-                DataAccessController.ContentCategory.CHROMATOGRAM,
+            // set data source name
+            this.setName(file.getName());
+            // set the type
+            this.setType(DataAccessController.Type.XML_FILE);
+            // set the content categories
+            this.setContentCategories(DataAccessController.ContentCategory.SPECTRUM,
                 DataAccessController.ContentCategory.SAMPLE,
                 DataAccessController.ContentCategory.INSTRUMENT,
                 DataAccessController.ContentCategory.SOFTWARE,
                 DataAccessController.ContentCategory.DATA_PROCESSING);
-        // create cache builder
-        setCacheBuilder(new MzXmlCacheBuilder(this));
-        // populate cache
-        populateCache();
+            // create cache builder
+            setCacheBuilder(new MzDataCacheBuilder(this));
+            // populate cache
+            populateCache();
+        } catch (JMzReaderException e) {
+            String msg = "Exception while create the MzData File";
+            logger.error(msg, e);
+            throw new DataAccessException(msg, e);
+
+        }
+
+
+
     }
 
     /**
@@ -116,7 +114,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
      *
      * @return MzMLUnmarshallerAdaptor mzML reader
      */
-    public MzXmlUnmarshallerAdaptor getUnmarshaller() {
+    public MzDataUnmarshallerAdaptor getUnmarshaller() {
         return unmarshaller;
     }
 
@@ -143,6 +141,59 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
         return uid;
     }
 
+    /**
+     * Get a list of cvlookups, these are not cached
+     *
+     * @return List<CvLookup>  a list of cvlookups
+     * @throws DataAccessException data access exception
+     */
+    public List<CVLookup> getCvLookups() throws DataAccessException {
+        try {
+            List<CvLookup> rawCvList = unmarshaller.getCvLookups();
+            return MzDataTransformer.transformCVList(rawCvList);
+        } catch (JMzReaderException e) {
+            String msg = "Exception while trying to read a list of cv lookups";
+            logger.error(msg, e);
+            throw new DataAccessException(msg, e);
+        }
+    }
+
+    /**
+     * Get referenceable paramgroup, this concept is only available in mzML
+     * It is a paramgroup with id
+     * And this is not cached
+     *
+     * @return ReferenceableParamGroup param group
+     * @throws DataAccessException data access exception
+     */
+    @Override
+    public ReferenceableParamGroup getReferenceableParamGroup() throws DataAccessException {
+       throw new UnsupportedOperationException("This method is not supported");
+    }
+
+    /**
+     * Get a list of samples by checking the cache first
+     *
+     * @return List<Sample>    a list of samples
+     * @throws DataAccessException data access exception
+     */
+    @Override
+    public List<Sample> getSamples() throws DataAccessException {
+        ExperimentMetaData metaData = super.getExperimentMetaData();
+
+        if (metaData == null) {
+            try {
+                Admin rawSample = unmarshaller.getAdmin();
+                return MzDataTransformer.transformSampleList(rawSample);
+            } catch (JMzReaderException e) {
+                String msg = "Exception while trying to read samples";
+                logger.error(msg, e);
+                throw new DataAccessException(msg, e);
+            }
+        } else {
+            return metaData.getSampleList();
+        }
+    }
 
     /**
      * Get a list of person contacts
@@ -153,10 +204,10 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
     @Override
     public List<Person> getPersonContacts() throws DataAccessException {
         try {
-            List<Operator> operators = unmarshaller.getPersonContacts();
+            List<uk.ac.ebi.pride.tools.mzdata_parser.mzdata.model.Person> rawFileDesc = unmarshaller.getPersonContacts();
             // List of Persons
-            return MzXmlTransformer.transformPersonContacts(operators);
-        } catch (MzXMLParsingException e) {
+            return MzDataTransformer.transformToPerson(rawFileDesc);
+        } catch (JMzReaderException e) {
             String msg = "Error while getting a list of person contacts";
             logger.error(msg, e);
             throw new DataAccessException(msg, e);
@@ -171,10 +222,10 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
     @Override
     public List<SourceFile> getSourceFiles() throws DataAccessException {
         try {
-            List<ParentFile> rawParentFiles = unmarshaller.getParentFiles();
-            // List of SourceFiles
-            return MzXmlTransformer.transformFileSources(rawParentFiles);
-        } catch (MzXMLParsingException e) {
+            uk.ac.ebi.pride.tools.mzdata_parser.mzdata.model.SourceFile rawFileDesc = unmarshaller.getSourceFiles();
+            // List of Persons
+            return MzDataTransformer.transformToFileSource(rawFileDesc);
+        } catch (JMzReaderException e) {
             String msg = "Error while getting a list of source files";
             logger.error(msg, e);
             throw new DataAccessException(msg, e);
@@ -184,7 +235,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
 
     @Override
     public ParamGroup getFileContent() throws DataAccessException {
-       throw new UnsupportedOperationException("This method is not supported");
+        throw new UnsupportedOperationException("This method is not supported");
     }
 
     @Override
@@ -193,9 +244,9 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
 
         if (metaData == null) {
             try {
-                List<uk.ac.ebi.pride.tools.mzxml_parser.mzxml.model.Software> rawSoftware = unmarshaller.getSoftwares();
-                return MzXmlTransformer.transformSoftwares(rawSoftware);
-            } catch (MzXMLParsingException e) {
+                uk.ac.ebi.pride.tools.mzdata_parser.mzdata.model.Software rawSoftware = unmarshaller.getSoftware();
+                return MzDataTransformer.transformSoftware(rawSoftware);
+            } catch (JMzReaderException e) {
                 String msg = "Error while getting a list of software";
                 logger.error(msg, e);
                 throw new DataAccessException(msg, e);
@@ -213,21 +264,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
      */
     @Override
     public List<ScanSetting> getScanSettings() throws DataAccessException {
-        MzGraphMetaData metaData = super.getMzGraphMetaData();
-
-        /*if (metaData == null) {
-            try {
-                ScanSettingsList rawScanSettingList = unmarshaller.getScanSettingsList();
-                return MzMLTransformer.transformScanSettingList(rawScanSettingList);
-            } catch (MzMLUnmarshallerException e) {
-                String msg = "Error while getting a list of scan settings";
-                logger.error(msg, e);
-                throw new DataAccessException(msg, e);
-            }
-        } else {
-            return metaData.getScanSettings();
-        }*/
-        return metaData.getScanSettings();
+        throw new UnsupportedOperationException("This method is not supported");
     }
 
     /**
@@ -242,9 +279,9 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
 
         if (metaData == null) {
             try {
-                List<MsInstrument> rawInstrumentList = unmarshaller.getMsInstruments();
-                return MzXmlTransformer.transformMsInstrument(rawInstrumentList);
-            } catch (MzXMLParsingException e) {
+                InstrumentDescription rawInstrumentList = unmarshaller.getInstrument();
+                return MzDataTransformer.transformInstrumentConfiguration(rawInstrumentList);
+            } catch (JMzReaderException e) {
                 String msg = "Error while getting a list of instrument configurations";
                 logger.error(msg, e);
                 throw new DataAccessException(msg, e);
@@ -264,11 +301,11 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
     public List<DataProcessing> getDataProcessings() throws DataAccessException {
         MzGraphMetaData metaData = super.getMzGraphMetaData();
 
-        /*if (metaData == null) {
+        if (metaData == null) {
             try {
-                uk.ac.ebi.jmzml.model.mzml.DataProcessingList rawDataProcList = unmarshaller.getDataProcessingList();
-                return MzMLTransformer.transformDataProcessingList(rawDataProcList);
-            } catch (MzMLUnmarshallerException e) {
+                uk.ac.ebi.pride.tools.mzdata_parser.mzdata.model.DataProcessing rawDataProcList = unmarshaller.getDataProcessing();
+                return MzDataTransformer.transformDataProcessing(rawDataProcList);
+            } catch (JMzReaderException e) {
                 String msg = "Error while getting a list of data processings";
                 logger.error(msg, e);
                 throw new DataAccessException(msg, e);
@@ -276,8 +313,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
         } else {
             return metaData.getDataProcessingList();
         }
-          */
-        return metaData.getDataProcessingList();
+
     }
 
     /**
@@ -304,10 +340,9 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
     Spectrum getSpectrumById(Comparable id, boolean useCache) throws DataAccessException {
         Spectrum spectrum = super.getSpectrumById(id, useCache);
         if (spectrum == null) {
-            uk.ac.ebi.pride.tools.jmzreader.model.Spectrum rawSpec = null;
             try {
-                rawSpec = unmarshaller.getSpectrumById(id.toString());
-                spectrum = MzXmlTransformer.transformSpectrum(rawSpec);
+                uk.ac.ebi.pride.tools.jmzreader.model.Spectrum rawSpec = unmarshaller.getSpectrumById(id.toString());
+                spectrum = MzDataTransformer.transformSpectrum(rawSpec);
                 if (useCache) {
                     getCache().store(CacheCategory.SPECTRUM, id, spectrum);
                 }
@@ -315,7 +350,6 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
                 logger.error("Get spectrum by id", ex);
                 throw new DataAccessException("Exception while trying to read Spectrum using Spectrum ID", ex);
             }
-
         }
         return spectrum;
     }
@@ -350,31 +384,44 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
 
         if (metaData == null) {
             // id , accession and version
-            String id = null;
-            String accession = null;
-            String version = null;
-            // SourceFile List
-            List<SourceFile> sourceFileList = getSourceFiles();
-            // List of Persons
-            List<Person> personList = getPersonContacts();
-            // List of Organizations
-            List<Organization> organizationList = null;
-            // Sample list
-            List<Sample> samples = null;
-            // Software list
-            List<Software> softwares = getSoftwares();
-            // ScanSettings list
-            ParamGroup fileContent = null;
-            metaData = new ExperimentMetaData(fileContent, id, accession, version, null, samples, softwares, personList, sourceFileList, null, organizationList, null, null, null, null);
+            try{
+                String id = unmarshaller.getAdmin().getSampleName();
+                String accession = unmarshaller.getAdmin().getSampleName();
+                String version = null;
+
+                // SourceFile List
+                List<SourceFile> sourceFileList = getSourceFiles();
+                // List of Persons
+                List<Person> personList = getPersonContacts();
+
+                // List of Organizations
+                List<Organization> organizationList = getOrganizationContacts();
+
+                // Sample list
+                List<Sample> samples = getSamples();
+                // Software list
+                List<Software> softwares = getSoftwares();
+                // ScanSettings list
+                ParamGroup fileContent = getFileContent();
+                metaData = new ExperimentMetaData(fileContent, id, accession, version, null, samples, softwares, personList, sourceFileList, null, organizationList, null, null, null, null);
+            }catch (JMzReaderException ex){
+                logger.error("Get spectrum by id", ex);
+                throw new DataAccessException("Exception while trying to read Experiment MetaData using Spectrum ID", ex);
+            }
         }
-
-
         return metaData;
     }
 
     @Override
     public MzGraphMetaData getMzGraphMetaData() throws DataAccessException {
-        return null;
+        MzGraphMetaData metaData = super.getMzGraphMetaData();
+        if (metaData == null) {
+            List<ScanSetting> scanSettings = getScanSettings();
+            List<DataProcessing> dataProcessings = getDataProcessings();
+            List<InstrumentConfiguration> instrumentConfigurations = getInstrumentConfigurations();
+            metaData = new MzGraphMetaData(null, null, scanSettings, instrumentConfigurations, dataProcessings);
+        }
+        return metaData;
     }
 
     @Override
@@ -399,7 +446,7 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
                 content.append(reader.readLine());
             }
 
-            Matcher matcher = mzXmlHeaderPattern.matcher(content);
+            Matcher matcher = mzDataHeaderPattern.matcher(content);
 
             valid = matcher.find();
         } catch (Exception e) {
@@ -416,4 +463,6 @@ public class MzXmlControllerImpl extends CachedDataAccessController {
 
         return valid;
     }
+
+
 }

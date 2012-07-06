535c77db35f74b09e67fba754224e53ded31483c
harry.r.wang@gmail.com <harry.r.wang@gmail.com@df3d61a3-6ab4-724c-a686-eaaef7b5d8fe>
2012-07-06 10:56:02 +0000

    Add help document for download ProteomeXchange submission

--- pride-inspector/branches/development/src/main/java/uk/ac/ebi/pride/gui/component/reviewer/PrideDownloadPane.java
+++ pride-inspector/branches/development/src/main/java/uk/ac/ebi/pride/gui/component/reviewer/PrideDownloadPane.java
@@ -103,7 +103,7 @@ public class PrideDownloadPane extends JPanel implements ActionListener {
         Icon helpIcon = GUIUtilities.loadIcon(context.getProperty("help.icon.small"));
         JButton helpButton = GUIUtilities.createLabelLikeButton(helpIcon, "Help");
         helpButton.setForeground(Color.blue);
-        CSH.setHelpIDString(helpButton, "help.download");
+        CSH.setHelpIDString(helpButton, "help.pride.download");
         helpButton.addActionListener(new CSH.DisplayHelpFromSource(context.getMainHelpBroker()));
         helpPane.add(helpButton);
 

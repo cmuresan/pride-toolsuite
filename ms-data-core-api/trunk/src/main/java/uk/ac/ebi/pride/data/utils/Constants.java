e8cc5afeed220d726925bd90720aedcd080b7a32
ypriverol <ypriverol@gmail.com>
2013-03-04 21:54:48 +0000

    Support for mzXML

--- ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/utils/Constants.java
+++ ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/utils/Constants.java
@@ -13,19 +13,25 @@ public class Constants {
     public static final String DTA_EXT = ".dta";
     public static final String MS2_EXT = ".ms2";
     public static final String PKL_EXT = ".pkl";
+    public static final String MZXML   = ".mzxml";
 
     /**
      *  Supported id format used in the spectrum file.
      *
      **/
-    public static enum SpecIdFormat{ MASCOT_QUERY_NUM, MULTI_PEAK_LIST_NATIVE_ID, SINGLE_PEAK_LIST_NATIVE_ID, MZML_ID, NONE
+    public static enum SpecIdFormat{
+        MASCOT_QUERY_NUM,
+        MULTI_PEAK_LIST_NATIVE_ID,
+        SINGLE_PEAK_LIST_NATIVE_ID,
+        SCAN_NUMBER_NATIVE_ID,
+        MZML_ID, NONE
     }
     /**
      * An enum of the supported spectra file types
      *
      **/
     public static enum SpecFileFormat {
-        MZML, PKL, DTA, MGF, NONE
+        MZML, PKL, DTA, MGF, MZXML,NONE
     }
 
 

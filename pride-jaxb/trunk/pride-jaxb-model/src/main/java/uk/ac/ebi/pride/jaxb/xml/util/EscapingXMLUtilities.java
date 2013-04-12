f294eb92ee4d093ab006e41907b4d0940aaf0090
rglcote <rglcote@gmail.com>
2013-04-12 13:57:01 +0000

    filtered out additional subset of control characters

--- pride-jaxb/trunk/pride-jaxb-model/src/main/java/uk/ac/ebi/pride/jaxb/xml/util/EscapingXMLUtilities.java
+++ pride-jaxb/trunk/pride-jaxb-model/src/main/java/uk/ac/ebi/pride/jaxb/xml/util/EscapingXMLUtilities.java
@@ -55,12 +55,49 @@ public class EscapingXMLUtilities {
          \u000A Line feed
          \u000D Carriage return
 
+         //valid, but discouraged
+         \u0080 	<control>
+         \u0081 	<control>
+         \u0082 	BREAK PERMITTED HERE
+         \u0083 	NO BREAK HERE
+         \u0084 	<control>
+         \u0085 	NEXT LINE (NEL)
+         \u0086 	START OF SELECTED AREA
+         \u0087 	END OF SELECTED AREA
+         \u0088 	CHARACTER TABULATION SET
+         \u0089 	CHARACTER TABULATION WITH JUSTIFICATION
+         \u008A 	LINE TABULATION SET
+         \u008B 	PARTIAL LINE FORWARD
+         \u008C 	PARTIAL LINE BACKWARD
+         \u008D 	REVERSE LINE FEED
+         \u008E 	SINGLE SHIFT TWO
+         \u008F 	SINGLE SHIFT THREE
+         \u0090 	DEVICE CONTROL STRING
+         \u0091 	PRIVATE USE ONE
+         \u0092 	PRIVATE USE TWO
+         \u0093 	SET TRANSMIT STATE
+         \u0094 	CANCEL CHARACTER
+         \u0095 	MESSAGE WAITING
+         \u0096 	START OF GUARDED AREA
+         \u0097 	END OF GUARDED AREA
+         \u0098 	START OF STRING
+         \u0099 	<control>
+         \u009A 	SINGLE CHARACTER INTRODUCER
+         \u009B 	CONTROL SEQUENCE INTRODUCER
+         \u009C 	STRING TERMINATOR
+         \u009D 	OPERATING SYSTEM COMMAND
+         \u009E 	PRIVACY MESSAGE
+         \u009F 	APPLICATION PROGRAM COMMAND
+
          */
 
         final String escapeString = "\u0000\u0001\u0002\u0003\u0004\u0005" +
-                "\u0006\u0007\u0008\u000B\u000C\u000E\u000F\u0010\u0011\u0012" +
+                "\u0006\u0007\u0008\u009D\u000B\u000C\u000E\u000F\u0010\u0011\u0012" +
                 "\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C" +
-                "\u001D\u001E\u001F\uFFFE\uFFFF";
+                "\u001D\u001E\u001F\uFFFE\uFFFF" +
+                "\u0080\u0081\u0082\u0083\u0084\u0085\u0086\u0087\u0088\u0089\u008A\u008B" +
+                "\u008C\u008D\u008E\u008F\u0090\u0091\u0092\u0093\u0094\u0095\u0096\u0097"  +
+                "\u0098\u0099\u009A\u009B\u009C\u009D\u009E\u009F";
 
         illegalChars = new CharOpenHashSet();
         for (int i = 0; i < escapeString.length(); i++) {

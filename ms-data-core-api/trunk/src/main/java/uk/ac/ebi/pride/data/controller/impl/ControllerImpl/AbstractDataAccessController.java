291b3695c8fa88595ae4c9c570274fbf2704788a
harry.r.wang@gmail.com <harry.r.wang@gmail.com@df3d61a3-6ab4-724c-a686-eaaef7b5d8fe>
2013-10-22 15:29:50 +0000

    change version number

--- ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/controller/impl/ControllerImpl/AbstractDataAccessController.java
+++ ms-data-core-api/trunk/src/main/java/uk/ac/ebi/pride/data/controller/impl/ControllerImpl/AbstractDataAccessController.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.pride.data.controller.impl.ControllerImpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.org.mozilla.javascript.IdScriptableObject;
 import uk.ac.ebi.pride.data.controller.DataAccessController;
 import uk.ac.ebi.pride.data.controller.DataAccessUtilities;
 import uk.ac.ebi.pride.data.core.*;
